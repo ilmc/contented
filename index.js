e6c5bba709c68592b509b1be27df3c70203610b4
Ian Moss <github@ilmc.co.uk>
2017-02-09 10:23:33 +0000

    Lots of changes

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+'use strict';
+
 var express = require('express')
 var app = express()
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('<h1>Contented</h1>')
-})
+var contented = require('./lib/index');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var swaggerTools = require('swagger-tools');
+
+var jsyaml = require('js-yaml');
+var fs = require('fs');
+
+//var options = {
+//  swaggerUi: '/swagger.json',
+//  controllers: __dirname + '/lib/controllers',
+//  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+//};
+
+var spec = fs.readFileSync(__dirname + '/lib/api/swagger.yaml', 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+contented.create( app, function (middleware) {
+
+  app.use('/admin', middleware.admin );
+
+  http.createServer(app).listen(3000, function () {
+    console.log('Contented listening on port 3000!')
+  });
+
+  // Start apps which depend on content here
+
+});
+
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+  app.use(middleware.swaggerUi());
+});
+
+
+
